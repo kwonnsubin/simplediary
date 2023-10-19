2917fbc4773595b0f57431c238f29bd17cc867e1
kwonnsubin <sbe3@daum.net>
2023-10-19 15:43:37 +0900

    simplediary

---
+++ src/App.js
@@ -0,0 +1,108 @@
+import React, { useCallback, useEffect, useMemo, useReducer, useRef } from 'react';
+import './App.css';
+import DiaryEditor from './DiaryEditor';
+import DiaryList from './DiaryList';
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case 'INIT': {
+      return action.data // 새로운 state가 된다.
+    }
+    case 'CREATE': {
+      const created_date = new Date().getTime();
+      const newItem = {
+        ...action.data,
+        created_date
+      }
+      return [newItem, ...state];
+    }
+    case 'REMOVE': {
+      return state.filter((it) => it.id !== action.targetId);
+    }
+    case 'EDIT': {
+      return state.map((it) => it.id === action.targetId ? { ...it, content: action.newContent } : it)
+    }
+    default:
+      return state;
+  }
+};
+
+export const DiaryStateContext = React.createContext();
+
+export const DiaryDispatchContext = React.createContext()
+
+function App() {
+  const [data, dispatch] = useReducer(reducer, []);
+
+  const dataId = useRef(0);
+
+  const getData = async () => { // Promise 반환
+    const res = await fetch('https://jsonplaceholder.typicode.com/comments')
+      .then((res) => res.json())
+
+    const initData = res.slice(0, 20).map((it) => {
+      return {
+        author: it.email,
+        content: it.body,
+        emotion: Math.floor(Math.random() * 5) + 1, // 1~5까지 랜덤으로
+        created_date: new Date().getTime(),
+        id: dataId.current++
+      }
+    });
+    dispatch({ type: "INIT", data: initData });
+    // setData(initData); // 2. initData state가 변경되면서 렌더링 일어남
+  };
+
+  useEffect(() => {
+    getData();
+  }, [])
+
+  const onCreate = useCallback((author, content, emotion) => {
+    dispatch({ type: 'CREATE', data: { author, content, emotion, id: dataId.current } })
+    dataId.current += 1;
+  }, // 작성완료 눌렀을 때 데이터 추가하는 함수
+    []
+  );
+
+  const onRemove = useCallback((targetId) => {
+    dispatch({ type: 'REMOVE', targetId });
+  }, []
+  );
+
+  const onEdit = useCallback((targetId, newContent) => {
+    dispatch({ type: 'EDIT', targetId, newContent });
+  }, []
+  );
+
+  const memoizedDispatches = useMemo(() => {
+    return { onCreate, onRemove, onEdit }
+  }, []);
+
+  const getDiaryAnalysis = useMemo(
+    () => {
+      const goodCount = data.filter((it) => it.emotion >= 3).length;
+      const badCount = data.length - goodCount;
+      const goodRatio = (goodCount / data.length) * 100;
+      return { goodCount, badCount, goodRatio }; // 객체로 반환
+    }, [data.length]
+  );
+
+  const { goodCount, badCount, goodRatio } = getDiaryAnalysis;
+
+  return (
+    <DiaryStateContext.Provider value={data}>
+      <DiaryDispatchContext.Provider value={memoizedDispatches}>
+        <div className="App">
+          <DiaryEditor onCreate={onCreate} />
+          <div>전체 일기 : {data.length}</div>
+          <div>기분 좋은 일기 개수 : {goodCount}</div>
+          <div>기분 나쁜 일기 개수 : {badCount}</div>
+          <div>기분 좋은 일기 비율 : {goodRatio}</div>
+          <DiaryList />
+        </div>
+      </DiaryDispatchContext.Provider>
+    </DiaryStateContext.Provider>
+  );
+}
+
+export default App;
